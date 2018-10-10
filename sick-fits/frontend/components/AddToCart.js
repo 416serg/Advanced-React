d7553cab6144707d1b5cc83fbdc240f8aa8c5be1
416serg <hey@416serg.me>
2018-10-10 16:56:49 -0400

    finished 43

---
+++ sick-fits/frontend/components/AddToCart.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
+
+const ADD_TO_CART_MUTATION = gql`
+  mutation ADD_TO_CART_MUTATION($id: ID!) {
+    addToCart(id: $id) {
+      id
+      quantity
+    }
+  }
+`;
+
+class AddToCart extends Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+  };
+
+  render() {
+    const { id } = this.props;
+    return (
+      <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ id }} refetchQueries={[{ query: CURRENT_USER_QUERY }]}>
+        {(addToCart, { error, loading }) => (
+          <button disabled={loading} type="button" onClick={addToCart}>
+            Add
+            {loading && 'ing'} To Cart 🛒
+          </button>
+        )}
+      </Mutation>
+    );
+  }
+}
+export default AddToCart;
---
+++ sick-fits/frontend/components/CartItem.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import RemoveFromCart from './RemoveFromCart';
+import formatMoney from '../lib/formatMoney';
+
+const CartItemStyles = styled.li`
+  padding: 1rem 0;
+  border-bottom: 1px solid ${props => props.theme.lightgrey};
+  display: grid;
+  align-items: center;
+  grid-template-columns: auto 1fr auto;
+  img {
+    margin-right: 10px;
+  }
+  h3,
+  p {
+    margin: 0;
+  }
+`;
+
+const CartItem = ({ cartItem: { quantity, id, item } }) => (
+  <CartItemStyles>
+    <img src={item.image} alt={item.title} width="100" />
+    <div className="cart-item-details">
+      <h3>{item.title}</h3>
+      <p>
+        {formatMoney(item.price * quantity)}
+        {` - `}
+        <em>
+          {quantity} &times; {formatMoney(item.price)}
+        </em>
+      </p>
+    </div>
+    <RemoveFromCart id={id} />
+  </CartItemStyles>
+);
+
+CartItem.propTypes = {
+  cartItem: PropTypes.object.isRequired,
+};
+
+export default CartItem;
